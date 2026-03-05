c4c356ecd75cbbc618888fd4e2ceeaf573f807ca
Taha Does That <141592180+tahadoesthat@users.noreply.github.com>
2026-03-05 11:40:03 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx' // Make sure this matches your file name
+import App from './app.jsx' // Note: Make sure your app file is exactly named app.jsx (case-sensitive!)
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
